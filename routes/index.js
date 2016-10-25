1de35140a06b79962d566de42278ed76235a86ba
zhanghaj00 <414080606@qq.com>
2016-10-25 13:38:31 +0800

    修改主页展示

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '张浩',comont:"欢迎来到我的接口" });
 });
 
 module.exports = router;
