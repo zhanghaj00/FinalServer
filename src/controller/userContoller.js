c1f7105fd5d533c8a645b062a0ea81dbe42ea05d
zhanghaj00 <414080606@qq.com>
2016-10-25 18:00:46 +0800

    修改返回结果

--- src/controller/userContoller.js
+++ src/controller/userContoller.js
@@ -10,13 +10,14 @@ var userDao = require('../model/user');
 exports.doLogin = function(req,res){
     var user = req.body;
     console.log('user'+user);
-
+    console.log('user'+user.username);
+    console.log('user'+user.password);
     userDao.login(user,function(err,username){
         if(err){
-            res.json({'result':false,'code':err});
+            res.json({'result':err,'code':1});
         }else{
             req.session.user = username;
-            res.json({'result':true,'code':'login success'});
+            res.json({'result':'login success','code':0});
         }
     })
 };
